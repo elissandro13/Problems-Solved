55566739ca6bc2a6cfcd03b5aba909e022aad78f
elissandro13 <ecjunior19@gmail.com>
2019-12-25 20:32:36 -0300

    Adding greed section on NOIC2

---
+++ NOIC2/gulosos/testeFunc.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string nome = "elissandro";
+    char nome2[12];
+
+    strcpy(nome2,"elissandro");
+
+    cout << nome.length() << "\n";
+    cout << nome.size() << "\n";
+    cout << strlen(nome2) << "\n";
+
+
+    return 0;
+}
